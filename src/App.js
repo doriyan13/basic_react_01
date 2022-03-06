a790b784d4f1a5189d72a8b6563e1af2516d807f
Doriyan <doriyan13@gmail.com>
2022-03-06 18:01:33 +0200

    starting part 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 // Imports -
-import Todo from "./components/Todo";
 
 /**
  * main, base function compoment.
  * @returns the return of jsx functions must return html elemnt!
  */
 function App() {
-  // Notice when i add css in js functions i must keep the special names of js!
-  // *for example instead of 'class' i will have to use 'className'! (it's not often that it's happening but i do have to notice that!)
-
-  /* Notice that you can add parameters to your custom compoments which you will
-      be able to-access with the props object and then get access to the fields/ params that i sent*/
-  /* Notice that you will use State to handle the switch between components */
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo title ="first" btnName="Check"/> 
-      <Todo title ="second" btnName="Delete"/>
-      <Todo title ="third" btnName="Add"/>
+        
     </div>
   );
 }
---
+++ src/example01/App_01.js
@@ -0,0 +1,28 @@
+// Imports -
+import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
+
+/**
+ * main, base function compoment.
+ * @returns the return of jsx functions must return html elemnt!
+ */
+function App() {
+  // Notice when i add css in js functions i must keep the special names of js!
+  // *for example instead of 'class' i will have to use 'className'! (it's not often that it's happening but i do have to notice that!)
+
+  /* Notice that you can add parameters to your custom compoments which you will
+      be able to-access with the props object and then get access to the fields/ params that i sent*/
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo title ="first" btnName="Check"/> 
+      <Todo title ="second" btnName="Delete"/>
+      <Todo title ="third" btnName="Add"/>
+      <Modal />
+      <Backdrop />
+    </div>
+  );
+}
+
+export default App;
