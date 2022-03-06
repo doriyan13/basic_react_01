df1fab3c4dacfdf7de93b49427376d77d12ae414
Doriyan <doriyan13@gmail.com>
2022-03-06 12:47:02 +0200

    starting react project and start basic component handling and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// Imports -
+import Todo from "./components/Todo";
 
+/**
+ * main, base function compoment.
+ * @returns the return of jsx functions must return html elemnt!
+ */
 function App() {
+  // Notice when i add css in js functions i must keep the special names of js!
+  // *for example instead of 'class' i will have to use 'className'! (it's not often that it's happening but i do have to notice that!)
+
+  /* Notice that you can add parameters to your custom compoments which you will
+      be able to-access with the props object and then get access to the fields/ params that i sent*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Todos</h1>
+      <Todo title ="first" btnName="Check"/> 
+      <Todo title ="second" btnName="Delete"/>
+      <Todo title ="third" btnName="Add"/>
     </div>
   );
 }
